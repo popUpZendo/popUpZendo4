d4feb70225c5ef951708ce4d6c9ad9724a121692
Benjamin Meyers <benjamin@purespiritwebworks.com>
2020-05-06 11:04:49 -0700

    changed file import method so the file is stored earlier in the process

--- sutra.php
+++ sutra.php
@@ -3,77 +3,42 @@
     <?php
       if(!isset($_POST['submit']) && !isset($_GET['sutra'])) {
     ?>
-      <div id="content">
-        <div id="main_content">
-          <h1>Sutra</h1>
-          <form action="sutra.php" method="POST">
-            <input type="text" name="sutra" id="sutra" value="sutra" placeholder="sutra">
-            <input type="submit" name="submit" id="submit" value="submit">
-          </form>
-        </div><!-- #main_content -->
-      </div><!-- #content -->
+    <div id="content">
+      <div id="main_content">
+        <h1>Sutra</h1>
+        <form action="sutra.php" method="POST">
+          <input type="text" name="sutra" id="sutra" value="sutra" placeholder="sutra">
+          <input type="submit" name="submit" id="submit" value="submit">
+        </form>
+      </div><!-- #main_content -->
+    </div><!-- #content -->
     <?php } else {
       if(isset($_POST['sutra'])) {
         $sutra = $_POST['sutra'];
       } elseif (isset($_GET['sutra'])) {
         $sutra = $_GET['sutra'];
       }
-      $sutraTitle = str_replace('_', ' ', $sutra)
+      $sutraTitle = str_replace('_', ' ', $sutra);
+      $sutraContentsString = file_get_contents ($sutra.'.txt');
+      $sutraContentsArray = file($sutra.'.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     ?>
-
-
-
 
     <div class="controls"><i class="fas fa-play"></i> Play | <i class="fas fa-stop"></i> Stop | <i class="fas fa-angle-double-down"></i> | <i class="fas fa-angle-double-up"></i></div>
     <h1><?php echo $sutraTitle; ?></h1>
     <hr>
-    <div id="txt"><span id="pre"></span><span id="post"></span></div>
+    <div id="txt"><span id="pre"><?php for ($l=0; $l<count($sutraContentsArray); $l++){ 
+      echo $sutraContentsArray[$l].'<br>'.'<hr>';
+    }
+    ?></span><span id="post"></span></div>
     <?php } ?>
+
     <script type="application/javascript">
-      var txtFile = new XMLHttpRequest();
-      var allText = "file not found";
       var arrayText = new Array;
+      arrayText = <?php echo json_encode($sutra.'.txt'); ?>;
       var speed = 800; // setting up speed variable
-
-      var txtFileTitle = <?php echo json_encode($sutra.'.txt'); ?>;
-      
-      txtFile.onreadystatechange = function () {
-        if (txtFile.readyState === XMLHttpRequest.DONE && txtFile.status == 200) {
-          allText = txtFile.responseText;
-          allText = allText.split("\n").join("<br><hr> ");
-          arrayText = allText.split(' ');
-        }
-
-        var wordPre = allText;
-        document.getElementById('pre').innerHTML = wordPre;
-        var wordCurrent = ''; // a number representing the current word position
-        var wordPost = ''; // the array of words after the current word
-        var displayText = arrayText;
-        var i = 0;
-
-        function showSutra(piStartpoint) {
-          if(i<arrayText.length){
-            wordPost += '<span class="wordPost">' + arrayText[i] + '</span> ';
-
-            document.getElementById('post').innerHTML = wordPost;
-            if(!piStartpoint){i++;}
-          }
-        }
-        setInterval(showSutra,speed);
-      }
-
-      function setSpeed(psDirection) {
-        if (!psDirection) { lsDirection = "increase" } else { lsDirection = psDirection } 
-        if(lsDirection == "increase") {
-          speed += 100;
-        } else {
-          speed = 100;
-        }
-        showSutra();
-      }
-      speed = 800;
-      txtFile.open("GET", txtFileTitle, true);
-      txtFile.send(null);
+      var allText = <?php echo json_encode($sutra); ?>;
+      allText = allText.split("\n").join("<br><hr> ");
+      arrayText = allText.split(' ');
     </script>
 
   </div><!-- #form -->
