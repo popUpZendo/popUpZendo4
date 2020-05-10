5bc6e8644241fe5dd3cd803d1f1dc9d0be2e659f
Benjamin Meyers <benjamin@purespiritwebworks.com>
2020-05-09 17:34:20 -0700

    #24 Sutra Speed Control

--- sutra.php
+++ sutra.php
@@ -24,7 +24,7 @@
     ?>
 
     <h1><?php echo $sutraTitle; ?>
-    <small class="controls"><a href="javascript:playSutra()"><i class="fas fa-play"></i> Play</a> | <a href="javascript:stopSutra()"><i class="fas fa-stop"></i> Stop</a></small></h1>
+    <small class="controls"><a href="javascript:playSutra()"><i class="fas fa-play"></i> Play</a> | <a href="javascript:stopSutra()"><i class="fas fa-stop"></i> Stop</a> | <a href="javascript:playSutra('50')"><i class="fas fa-angle-double-up"></i></a> | <a href="javascript:playSutra('-50')"><i class="fas fa-angle-double-down"></i></a> </small></h1>
     <hr>
     <div id="txt"><span id="pre"></span><span id="post"></span></div>
     <?php } ?>
@@ -32,32 +32,51 @@
     <script type="text/javascript">
       var sutraArray = new Array;
       sutraArray = <?php echo json_encode($sutra.'.txt'); ?>;
-      var speed = 800; // setting up speed variable
+      var speed = 600; // setting up speed variable
       var sutraText = <?php echo json_encode($sutraContentsString); ?>;
       sutraText = sutraText.split("\n").join("<br><hr> ");
+      sutraText = sutraText.split("|").join("");
       sutraArray = sutraText.split(' ');
       var wordCurrent = 0;
       var wordsRead = '';
+      var wordsComposed = '';
       var startWord = '';
-      var inerval = '';
+      var interval = '';
 
-      document.getElementById('pre').innerHTML = sutraText;
+      function buildInitialSutra() {
+        for(var word = 0;word<sutraArray.length;word++) {
+          wordsComposed += '<span class="wordIndividual wordPre" id="word-' + word + '">' + sutraArray[word] + ' </span>';
+          document.getElementById('pre').innerHTML = wordsComposed;
+        }
+      }
 
-      function showSutra() {
+      buildInitialSutra();
+
+      function showSutra() { // build the post words string
         if(wordCurrent<sutraArray.length) {
-          wordsRead += '<span class="wordPost">' + sutraArray[wordCurrent] + ' </span>';
+          wordsRead += '<span class="wordIndividual wordPost" id="word-' + wordCurrent + '">' + sutraArray[wordCurrent] + ' </span>';
           document.getElementById('post').innerHTML = wordsRead;
         }
         wordCurrent++;
       }
 
-      function playSutra(){
-        interval = setInterval("showSutra()",speed);
+      function playSutra(speedChange){
+        stopSutra();
+        if(!speedChange) {
+          speed = speed;
+        } else {
+          if(Math.sign(speedChange) == 1) { // decrease
+            speed = speed - 50;
+          } else {
+            speed += 50;
+          }
+        }
+        interval = setInterval("showSutra()", speed);  
       }
 
       function stopSutra(){
         clearInterval(interval);
-      }
+      }=
     </script>
 
   </div><!-- #form -->
@@ -68,6 +87,7 @@
     #post {z-index: 2;}
     #post span {color:#eee;}
     #post span:last-child {color:#111;}
+ /*   #txt span {cursor:pointer;} */
     .controls {background:#eee;border-radius:10px;padding:10px;position:fixed;z-index:10;
     }
   </style>
